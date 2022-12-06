0b320e0016de027733b239740f540993d45033a7
TemnikovES <114111362+TemnikovES@users.noreply.github.com>
2022-12-06 16:55:17 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,27 @@
+class AuthEmail:
+    email = 'qatestrt@rambler.ru'
+    password = 'Qwer123123'
+
+class AuthPhone:
+    phone = '9522855569'
+    password = 'Qwer123123'
+
+class AuthLogin:
+    login = 'rtkid_1670145210607'
+    password = 'Qwer123123'
+
+class InvalidData:
+    login = 'login_non_true'
+    password = "Qwert12345"
+
+class SymbolData:
+    login = '/*!@#$'
+    password = "Qwert12345"
+
+class KirillData:
+    login = 'Кириллица'
+    password = "Кириллица"
+
+class BIGData:
+    login = 'login'
+    password = "passw"
